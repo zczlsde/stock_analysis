7147bf36c0b57a5588c29823018a09aa34349bfa
zczlsde <zczlsde@ucl.ac.uk>
2021-01-15 17:29:14 +0800

    remove_all

--- search.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('stock-data.db')
-c= conn.cursor()
-c.execute("SELECT id, time, code, description from SZ000002 where HIGH<10.15 and HIGH >10.12")
-for row in c:
-    print("ID = {}; TIME = {};CODE = {}; DESCRIPTION = {};".format (row[0],row[1],row[2],row[3]))
--- update.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('stock-data.db')
-c= conn.cursor()
-c.execute("UPDATE SZ000002 set DESCRIPTION = 'NONE' WHERE ID = 1")
-conn.commit()
-c.execute("select * from SZ000002")
-print(c.fetchall())
